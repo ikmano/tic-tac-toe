3f3694f4456c1bfef7f7a8ece6309b7392d10db6
Monika <ham.mon.00@gmail.com>
2021-10-17 17:48:46 +0200

    functioning app

--- app.js
+++ app.js
@@ -13,53 +13,58 @@ const GameBoard = (() => {
         }
     };
 
+    const resetBoard = () => {
+        console.log(htmlBoard);
+        while (htmlBoard.firstChild) {
+            htmlBoard.firstChild.remove()
+        }
+        makeBoard();
+    }
+
+    const freezeBoard = () => {
+        const div = document.createElement('div');
+        div.setAttribute('id','freeze');
+        htmlBoard.appendChild(div);
+    }
+
     return {
         makeBoard,
+        resetBoard,
+        freezeBoard,
     }
 })();
 
 GameBoard.makeBoard();
-const tiles = document.querySelectorAll('.tile');
-//console.log(tiles);
 // GAMEFLOW 
 
 const GameControl = (() => {
-    
     const WINNING_COMBS = [
         [0,1,2],[3,4,5],[6,7,8],
         [2,5,8],[1,7,4],[0,3,6],
         [0,4,8],[6,4,2]
     ]
-
-    let lastTurn = "O";
-    
-    const getCleanBoard = () => {
+    const msg = document.getElementById('msg');
+   
+    const getCleanBoard = (tiles) => {
         let boardMembers = [];
-        tiles.forEach(e => {
-            boardMembers.push(e.innerHTML);
+        tiles.forEach(tile => {
+            boardMembers.push(tile.innerHTML);
         });
+        console.log(boardMembers);
         return boardMembers;
     }
     
-    let marked = getCleanBoard();
-
-    function freezeBoard(mark){
-        tiles.forEach(e => {
-            e.removeEventListener('mousedown', mark);
-        });
-    };
-    
-    function checkWinner(){
+    function checkWinner(marked){
         let won = false;
         WINNING_COMBS.forEach(x => {
             if(marked[x[0]] === marked[x[1]]) {
                 if(marked[x[0]] === marked[x[2]]) {
                     if(marked[x[0]] === "X") {
-                        document.getElementById('msg').innerHTML= "Congratulations X's won!";
+                        msg.innerHTML= "Congratulations X's won!";
                         won = true;
                     }                        
                     else if(marked[x[0]] === 'O') {
-                        document.getElementById('msg').innerHTML= "Congratulations O's won!";
+                        msg.innerHTML= "Congratulations O's won!";
                         won = true;
                     }
                 }
@@ -68,44 +73,62 @@ const GameControl = (() => {
         return won;
     }
 
-    function endGame(won, mark) {
-        if(won) freezeBoard(mark);
+    function endGame(won, marked) {
+        if(won) {
+            GameBoard.freezeBoard();
+        }
         else {
             let i = 0;
             marked.forEach(e => {
                 if(e === "") i++;
             });   
             if(i === 0) {
-                document.getElementById('msg').innerHTML= "It's a draw";
-                freezeBoard(mark);
+                msg.innerHTML= "It's a tie";
+                GameBoard.freezeBoard();
             }
         }
     }
 
-    let index;
-    let won;
-    
+    const start = () => {
+        const tiles = document.querySelectorAll('.tile');
+        let marked = getCleanBoard(tiles);
+        console.log(marked);
+        let lastTurn = "O";
+        let index;
+        let won;
+
         tiles.forEach(tile => {
-            tile.addEventListener('mousedown', function mark(){
+            tile.addEventListener('mousedown', () => {
                 index = Array.from(tile.parentNode.children).indexOf(tile);
-
-            if(marked[index] === "") {
-                if(lastTurn === "O") {
-                    marked[index] = 'X';
-                    tile.innerHTML = "X";
-                    lastTurn = "X";
+                if(marked[index] === "") {
+                    if(lastTurn === "O") {
+                        marked[index] = "X";
+                        tile.innerHTML = "X";
+                        lastTurn = "X";
+                    }
+                    else {
+                        marked[index] = "O";
+                        tile.innerHTML = "O";
+                        lastTurn = "O";
+                    }   
+                    won = checkWinner(marked);        
+                    endGame(won, marked);
                 }
-                else {
-                    marked[index] = 'O';
-                    tile.innerHTML = "O";
-                    lastTurn = "O";
-                }   
-                won = checkWinner();        
-                endGame(won, mark);
-            }
+          });
         });
-});
+    }
 
+    const reset = () => {
+        GameBoard.resetBoard();
+        msg.innerHTML= "";
+        start();
+    }
+
+    return {
+        start,
+        reset,
+    }
 
 })();
 
+GameControl.start();
